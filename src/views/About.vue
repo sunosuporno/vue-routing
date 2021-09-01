22b42edfde77ce6519051efee7edf101e32df5ef
sunosuporno <sarkarsuporno36@gmail.com>
2021-09-01 23:57:35 +0530

    learnt a lot tbh

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt culpa
+      porro fugiat, dolorem ipsa, consectetur placeat, quos praesentium optio
+      eius modi ea neque consequatur inventore aliquid eum voluptatem
+      perspiciatis sint. Suscipit hic ducimus enim quidem nisi beatae voluptates
+      odio ipsam dolor. Cumque harum, est ipsum nihil enim, nesciunt beatae illo
+      minima repellat, odit numquam? Amet iste obcaecati eos blanditiis nihil
+      possimus deserunt ipsa architecto ut. Facilis quae doloremque veniam
+      dolores voluptatem assumenda nam, aliquid veritatis dicta similique quis,
+      magnam quod autem eius amet culpa? Temporibus laudantium, ducimus corrupti
+      vitae ab explicabo officiis quidem repellendus in, ipsam perspiciatis
+      doloribus voluptates! Atque.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt culpa
+      porro fugiat, dolorem ipsa, consectetur placeat, quos praesentium optio
+      eius modi ea neque consequatur inventore aliquid eum voluptatem
+      perspiciatis sint. Suscipit hic ducimus enim quidem nisi beatae voluptates
+      odio ipsam dolor. Cumque harum, est ipsum nihil enim, nesciunt beatae illo
+      minima repellat, odit numquam? Amet iste obcaecati eos blanditiis nihil
+      possimus deserunt ipsa architecto ut. Facilis quae doloremque veniam
+      dolores voluptatem assumenda nam, aliquid veritatis dicta similique quis,
+      magnam quod autem eius amet culpa? Temporibus laudantium, ducimus corrupti
+      vitae ab explicabo officiis quidem repellendus in, ipsam perspiciatis
+      doloribus voluptates! Atque.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt culpa porro fugiat, dolorem ipsa, consectetur placeat, quos praesentium optio eius modi ea neque consequatur inventore aliquid eum voluptatem perspiciatis sint. Suscipit hic ducimus enim quidem nisi beatae voluptates odio ipsam dolor. Cumque harum, est ipsum nihil enim, nesciunt beatae illo minima repellat, odit numquam? Amet iste obcaecati eos blanditiis nihil possimus deserunt ipsa architecto ut. Facilis quae doloremque veniam dolores voluptatem assumenda nam, aliquid veritatis dicta similique quis, magnam quod autem eius amet culpa? Temporibus laudantium, ducimus corrupti vitae ab explicabo officiis quidem repellendus in, ipsam perspiciatis doloribus voluptates! Atque.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt culpa porro fugiat, dolorem ipsa, consectetur placeat, quos praesentium optio eius modi ea neque consequatur inventore aliquid eum voluptatem perspiciatis sint. Suscipit hic ducimus enim quidem nisi beatae voluptates odio ipsam dolor. Cumque harum, est ipsum nihil enim, nesciunt beatae illo minima repellat, odit numquam? Amet iste obcaecati eos blanditiis nihil possimus deserunt ipsa architecto ut. Facilis quae doloremque veniam dolores voluptatem assumenda nam, aliquid veritatis dicta similique quis, magnam quod autem eius amet culpa? Temporibus laudantium, ducimus corrupti vitae ab explicabo officiis quidem repellendus in, ipsam perspiciatis doloribus voluptates! Atque.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
